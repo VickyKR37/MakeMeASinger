f6a472d357ea4496b9d7484b3411340edae14925
vicky <vickyks@hotmail.com>
2022-04-29 14:28:13 +0000

    added a test for service_2 which passed

--- service_2/tests/test_service_2.py
+++ service_2/tests/test_service_2.py
@@ -1,1 +1,18 @@
+from flask import url_for
+from flask_testing import TestCase
 
+from service_2.app import app, name
+
+class TestBase(TestCase):
+    def create_app(self):
+        return app
+
+class TestResponse(TestBase):
+
+    def test_get_name(self):
+
+        for i in range(20):
+            response = self.client.get(url_for('get_name'))
+
+            self.assert200(response)
+            self.assertIn(response.data.decode(), name)
