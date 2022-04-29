eeaab0ea9ebdd372f41c6c24c4c86900ab99d625
vicky <vickyks@hotmail.com>
2022-04-29 14:40:25 +0000

    added a test folder, file and test to service 3

--- service_3/tests/test_service_3.py
+++ service_3/tests/test_service_3.py
@@ -1,7 +1,7 @@
 from flask import url_for
 from flask_testing import TestCase
 
-from service_2.app import app, name
+from service_3.app import app
 
 class TestBase(TestCase):
     def create_app(self):
@@ -9,10 +9,8 @@ class TestBase(TestCase):
 
 class TestResponse(TestBase):
 
-    def test_get_name(self):
-
         for i in range(20):
-            response = self.client.get(url_for('get_name'))
+            response = self.client.get(url_for('get_surname'))
 
             self.assert200(response)
-            self.assertIn(response.data.decode(), name)
+            self.assertIn(int(response.data.decode()), surname)
