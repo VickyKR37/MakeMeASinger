f0b40f98440dee7c40ea344278bba8be47349deb
vicky <vickyks@hotmail.com>
2022-05-04 15:22:18 +0000

    app is now rendering on the browser because I deleted the base.html and removed the extends... and I also added .text to the name and surname in the statement

--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -2,11 +2,11 @@ from flask import render_template, redirect, url_for, request
 import requests, json
 from . import app
 
-@app.route('/', methods=["POST", "GET"])
+@app.route('/home', methods=["POST", "GET"])
 def index():
     name = requests.get('http://service_2:5000/get/name')
     surname = requests.get('http://service_3:5000/get/surname')
-    statement = f"Your name is {name} {surname}"
+    statement = f"Your name is {name.text} {surname.text}"
 
     return render_template('index.html', statement=statement)
 
