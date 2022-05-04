ed328e87b61e893dbd70b93ead4cf19436e8ea2a
vicky <vickyks@hotmail.com>
2022-05-04 13:44:08 +0000

    modified app.py and routes.py

--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -5,7 +5,7 @@ from . import app
 @app.route('/', methods=["POST", "GET"])
 def index():
     name = requests.get('http://service_2:5000/get/name')
-    surname = requests.get('http://service_3:5000/get/name')
+    surname = requests.get('http://service_3:5000/get/surname')
     statement = f"Your name is {name} {surname}"
 
     return render_template('index.html', statement=statement)
