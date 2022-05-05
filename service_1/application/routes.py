bcc88f43ae2e9a37ff3b5fe8b4f773010650afae
vicky <vickyks@hotmail.com>
2022-05-05 10:43:12 +0000

    VR: added a genre variable to the function and to the statement

--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -6,7 +6,8 @@ from . import app
 def index():
     name = requests.get('http://service_2:5000/get/name')
     surname = requests.get('http://service_3:5000/get/surname')
-    statement = f"Your name is {name.text} {surname.text}"
+    genre = requests.get('http://service_4:5000/get/genre')
+    statement = f"Your name is {name.text} {surname.text} and the genre you sing is {genre.text}."
 
     return render_template('index.html', statement=statement)
 
