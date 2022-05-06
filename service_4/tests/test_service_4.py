64a99e398fc2c182f970a1ee1b6c5c3e92879db4
vicky <vickyks@hotmail.com>
2022-05-06 14:45:03 +0000

    VR: added a test for service_4 and it passed

--- service_4/tests/test_service_4.py
+++ service_4/tests/test_service_4.py
@@ -1,18 +1,21 @@
-# from application import app
-# import requests
-# import requests_mock
-# from requests_mock import mock
-# from flask import url_for
-# from flask_testing import TestCase
+from app import app, get_genre
+import requests
+import requests_mock
+from requests_mock import mock
+from flask import url_for
+from flask_testing import TestCase
 
-# class TestBase(TestCase):
-#     def create_app(self):
-#         return app
+class TestBase(TestCase):
+    def create_app(self):
+        return app
 
-# class TestResponse(TestBase):
+class TestResponse(TestBase):
 
-#     def test_get_genre():
-#         response = 
+    def test_get_genre(self):
+        response = self.client.post(url_for('get_genre'),
+        json = dict(name="Harry", surname="Styles"))
+        self.assertIn(b'RnB', response.data)
+
 
 
 
