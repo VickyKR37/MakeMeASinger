6c43f53b67d4d69df10ffeab3bd76cfaf4271907
vicky <vickyks@hotmail.com>
2022-05-06 10:23:05 +0000

    added a test to service_1 but it is yet to be fully completed

--- service_1/tests/test_service_1.py
+++ service_1/tests/test_service_1.py
@@ -1,4 +1,13 @@
 import requests
 import requests_mock
 from requests_mock import mock
-from app import app, name, surname, get_genre, get_json
+from app import app, index, name, surname, get_genre
+
+def test_index():
+    with requests_mock.Mocker() as m:
+        m.get('http://service_2:5000/get/name', text='Zara')
+        m.get('http://service_3:5000/get/surname', text='Larsson')
+        m.post('http://service_4:5000/post/genre', json={"fullname":"Zara Larsson"})
+        assert index()["name"] == "Zara"
+        assert index()["surname"] == "Larsson"
+        assert inxed()["genre"] == 
