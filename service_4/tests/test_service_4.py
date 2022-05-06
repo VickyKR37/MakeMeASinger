5ff36f5f5bc9418f85c82b0d56b20e05ad539bf4
vicky <vickyks@hotmail.com>
2022-05-06 13:14:33 +0000

    VR: added a test to service_1 which passed

--- service_4/tests/test_service_4.py
+++ service_4/tests/test_service_4.py
@@ -1,3 +1,18 @@
+# from application import app
+# import requests
+# import requests_mock
+# from requests_mock import mock
+# from flask import url_for
+# from flask_testing import TestCase
+
+# class TestBase(TestCase):
+#     def create_app(self):
+#         return app
+
+# class TestResponse(TestBase):
+
+#     def test_get_genre():
+#         response = 
 
 
 
